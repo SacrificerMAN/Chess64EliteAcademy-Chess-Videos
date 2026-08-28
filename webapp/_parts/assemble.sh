a02414853262e21e7d0b9a325dd442e53f9ff3f5
SacrificerMAN <mokusochess@gmail.com>
2026-08-28 16:26:26 +0530

    Fix assemble.sh to base64-decode A0-A6 and H0-H5 into app.py + index.html

--- webapp/_parts/assemble.sh
+++ webapp/_parts/assemble.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 mkdir -p webapp/static
-cat webapp/_parts/app.0 webapp/_parts/app.1 webapp/_parts/app.2 webapp/_parts/app.3 > webapp/app.py
-cat webapp/_parts/html.0 webapp/_parts/html.1 webapp/_parts/html.2 webapp/_parts/html.3 > webapp/static/index.html
-echo "assembled webapp v2"
+# App.py from base64 parts A0-A6
+cat webapp/_parts/A0 webapp/_parts/A1 webapp/_parts/A2 webapp/_parts/A3 webapp/_parts/A4 webapp/_parts/A5 webapp/_parts/A6 | base64 -d > webapp/app.py
+# index.html from base64 parts H0-H5
+cat webapp/_parts/H0 webapp/_parts/H1 webapp/_parts/H2 webapp/_parts/H3 webapp/_parts/H4 webapp/_parts/H5 | base64 -d > webapp/static/index.html
+echo "assembled webapp v2 (A0-A6 + H0-H5)"
+ls -la webapp/app.py webapp/static/index.html
